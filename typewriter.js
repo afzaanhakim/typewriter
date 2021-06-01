88010be496cec41519f6f395276c165323135731
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-01 17:29:23 +0000

    playing around with process.stdout.write and pushing elements on a new line

--- typewriter.js
+++ typewriter.js
@@ -2,14 +2,16 @@
 let string = "hello there from lighthouse labs";
 let slowWriter = (string) => {
   let seconds = 1000;
-  let element;
-  for (element of string) {
-    let x = element;
+
+  for (let element of string) {
     setTimeout (()=> {
-      process.stdout.write([x] + '\n' )
-    }, seconds)  
+    process.stdout.write((element) + '\n')
+    
+    }, seconds) 
     seconds += 50;
+  
   }
+  
 };
 
 slowWriter(string);
