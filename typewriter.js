89d95b6e7aa921070778a4317079a1c77ac965ff
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-05 23:44:08 +0000

    made edits to add newline at the end of the animation

--- typewriter.js
+++ typewriter.js
@@ -1,17 +1,14 @@
-
 let string = "hello there from lighthouse labs";
+
 let slowWriter = (string) => {
   let seconds = 1000;
-
+  string += "\n";
   for (let element of string) {
-    setTimeout (()=> {
-    process.stdout.write((element) + '\n')
-    
-    }, seconds) 
+    setTimeout(() => {
+      process.stdout.write(element);
+    }, seconds);
     seconds += 50;
-  
   }
-  
 };
 
 slowWriter(string);
